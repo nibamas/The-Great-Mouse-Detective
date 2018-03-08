ed502edca3f586c9f6ea7d222137cfc3cfbf59f3
Niba <imeldaniba@gmail.com>
2018-03-08 13:43:46 +0000

    fyrsta test komið

---
+++ textUtilities.js
@@ -0,0 +1,10 @@
+// require fer ofan í node_modules möppuna og sækir chai þar
+var expect = require('chai').expect;
+
+
+var titleCase = (name) => {
+  return "";
+}
+
+
+expect(titleCase('the great mouse detective')).to.be.a('string');
