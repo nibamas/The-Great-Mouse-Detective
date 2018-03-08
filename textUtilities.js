8c17d2d5701faf374c0f10a92c492a817bbb35af
Niba <imeldaniba@gmail.com>
2018-03-08 14:04:58 +0000

    virkar með einu orði

--- textUtilities.js
+++ textUtilities.js
@@ -3,11 +3,11 @@ var expect = require('chai').expect;
 
 
 var titleCase = (name) => {
-  return name.toUpperCase();
+  return name[0].toUpperCase() + name.substring(1);
 }
-
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
 expect(titleCase('a')).to.be.equal('A');
+expect(titleCase('matrix')).to.be.equal('Matrix');
 
 expect(titleCase('the great mouse detective')).to.be.equal('The Great Mouse Detective');
