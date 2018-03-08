b791602b4cbd22c5f88850d5e3fa81c70104db6c
Niba <imeldaniba@gmail.com>
2018-03-08 14:18:04 +0000

    hvert orð fyrir sig lagað

--- textUtilities.js
+++ textUtilities.js
@@ -3,8 +3,14 @@ var expect = require('chai').expect;
 
 
 var titleCase = (name) => {
-  return name[0].toUpperCase() + name.substring(1);
+  let words = name.split(" ");
+  let upperwords = words.map( (word) => {
+    return word[0].toUpperCase() + word.substring(1);
+  });
+  let finalname = upperwords.join(' ');
+  return finalname;
 }
+
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
 expect(titleCase('a')).to.be.equal('A');
